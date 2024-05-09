7bc91f3fe38efb13b1a45b84041278e503d8de7b
manishKrMahto <manishcode123@gmail.com>
2024-05-09 18:48:42 +0530

    spelling currected

--- sum.py
+++ sum.py
@@ -4,6 +4,6 @@ num2 = int(input('Enter second nummber : '))
 sum = num1 + num2 
 sub = num1 - num2
 
-printf('my result :')
+print('my result :')
 print(f'{num1} + {num2} = {sum}')
 print(f'{num1} - {num2} = {sub}')
