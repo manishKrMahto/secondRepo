ec60e0b4163f79c60656aba26617607f29eac72a
manishKrMahto <manishcode123@gmail.com>
2024-05-09 18:47:57 +0530

    added my result line

--- product.py
+++ product.py
@@ -4,5 +4,6 @@ num2 = int(input('Enter second nummber : '))
 product = num1 * num2 
 division = num1 / num2
 
+printf('my result :')
 print(f'{num1} * {num2} = {product}')
 print(f'{num1} / {num2} = {division}')
--- sum.py
+++ sum.py
@@ -4,5 +4,6 @@ num2 = int(input('Enter second nummber : '))
 sum = num1 + num2 
 sub = num1 - num2
 
+printf('my result :')
 print(f'{num1} + {num2} = {sum}')
 print(f'{num1} - {num2} = {sub}')
